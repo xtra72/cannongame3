2a0f8adc05d0bf891c95e2da0d15ac0b4bc1424e
xtra <xtra72@gmail.com>
2023-07-28 13:24:19 +0900

    Motion class 추가

---
+++ src/main/java/com/nhnacademy/Motion.java
@@ -0,0 +1,41 @@
+package com.nhnacademy;
+
+public class Motion {
+    int dx;
+    int dy;
+
+    Motion(int dx, int dy) {
+        this.dx = dx;
+        this.dy = dy;
+    }
+
+    public static Motion createPosition(int dx, int dy) {
+        return new Motion(dx, dy);
+    }
+
+    public static Motion createDisplacement(int magnitude, int theta) {
+        return new Motion((int) (magnitude * Math.cos(Math.toRadians(theta))),
+                (int) (magnitude * Math.sin(Math.toRadians(theta))));
+    }
+
+    public int getDX() {
+        return dx;
+    }
+
+    public int getDY() {
+        return dy;
+    }
+
+    public int getMagnitude() {
+        return (int) Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2));
+    }
+
+    public int getTheta() {
+        return (int) Math.toDegrees(Math.atan2(dy, dx));
+    }
+
+    public void add(Motion other) {
+        dx += other.getDX();
+        dy += other.getDY();
+    }
+}
---
+++ src/test/java/com/nhnacademy/MotionTest.java
@@ -0,0 +1,47 @@
+package com.nhnacademy;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Random;
+
+import org.junit.Test;
+import org.junit.jupiter.api.RepeatedTest;
+
+public class MotionTest {
+    Random random = new Random();
+
+    @RepeatedTest(100)
+    public void createPosition() {
+        int dx = random.nextInt(100);
+        int dy = random.nextInt(100);
+
+        Motion motion = Motion.createPosition(dx, dy);
+
+        assertEquals(dx, motion.getDX());
+        assertEquals(dy, motion.getDY());
+    }
+
+    @RepeatedTest(100)
+    public void addMotion() {
+        int dx1 = random.nextInt(100);
+        int dy1 = random.nextInt(100);
+        int dx2 = random.nextInt(100);
+        int dy2 = random.nextInt(100);
+
+        Motion motion1 = Motion.createPosition(dx1, dy1);
+        Motion motion2 = Motion.createPosition(dx2, dy2);
+
+        motion1.add(motion2);
+
+        assertEquals(dx1 + (long) dx2, motion1.getDX());
+        assertEquals(dy1 + (long) dy2, motion1.getDY());
+    }
+
+    @Test
+    public void comparePositionVsDisplacement() {
+        Motion motion = Motion.createPosition(30, 40);
+
+        assertEquals(50, motion.getMagnitude());
+        assertEquals(60, motion.getTheta());
+    }
+}
